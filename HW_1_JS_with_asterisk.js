e95ac046cd254fa7ee3b258b4ef5d7f7ed731732
avkpro2022 <avkpro@gmail.com>
2022-06-19 16:21:50 +0300

    HW with asterisk

---
+++ HW_1_JS_with_asterisk.js
@@ -0,0 +1,38 @@
+let age_1 = Number
+
+let age_2 = 18
+
+let age_3 = 60
+
+let checkAge = function(age_1) {
+    if (age_1 < age_2) {
+        console.log(`“You don’t have access cause your age is ”` + age_1 + `“ It’s less then”`)
+    } else if (age_1 >= age_2 && age_1 < age_3) {
+        console.log("Welcome")
+    }}
+
+
+    checkAge(6)
+
+// if (age_1 < age_2)
+// {console.log(`“You don’t have access cause your age is ”` + age_1 + `“ It’s less then”`)}
+// // Если age_1 < age_2, вывести в консоль “You don’t have access cause your age is” + age_1 + “It’s less then”
+// else if (age_1 >= age_2, age_1 < age_3)
+// {console.log("Welcome")}
+// // Если age_1 >=  age_2 и age_1 <  age_3, вывести в консоль “Welcome!"
+//  else if 
+//  (age_1 > age_3){console.log("Keep calm and look Culture channe")}
+// //  Если age_1  > age_3, вывести в консоль “Keep calm and look Culture channel”.
+// else {console.log("Technical work")}
+// // Иначе выводите “Technical work”
+
+// const checkage = function(age) {
+
+
+
+
+// Преобразовать написанный код в 26-33 пунктах в функцию, принимающую на вход возраст.
+// Пример: const checkAge = function(age) {
+// Ваши преобразования
+// }
+// Вывести в консоль результат работы функции с возрастами 17, 18, 61
