7fd0d4926b40b358ba24cca05b2d84d641c282c4
avkpro2022 <avkpro@gmail.com>
2022-06-19 15:16:19 +0300

    corrected string 50 with improvement

--- HW.js
+++ HW.js
@@ -47,7 +47,7 @@ let age_2 = 18
 let age_3 = 60
 // Создать переменную “age_3” и присвоить ей значение 60
 if (age_1 < age_2)
-{console.log(`“You don’t have access cause your age is ” + age_1 + “ It’s less then”`)}
+{console.log(`“You don’t have access cause your age is ”` + age_1 + `“ It’s less then”`)}
 // Если age_1 < age_2, вывести в консоль “You don’t have access cause your age is” + age_1 + “It’s less then”
 else if (age_1 >= age_2, age_1 < age_3)
 {console.log("Welcome")}
