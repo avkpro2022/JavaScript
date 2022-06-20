166542b9e1336dd33eb4c5de0bb5a0ec2c59f74a
avkpro2022 <avkpro@gmail.com>
2022-06-20 20:55:38 +0300

    continue work

--- HW_1_JS_with_asterisk.js
+++ HW_1_JS_with_asterisk.js
@@ -1,38 +1,54 @@
-let age_1 = Number
+// let age_1 = Number
+
+// let age_2 = 18
+
+// let age_3 = 60
+
+// let checkAge = function(age_1) {
+//     if (age_1 < age_2) {
+//         console.log(`“You don’t have access cause your age is ”` + age_1 + `“ It’s less then”`)
+//     } else if (age_1 >= age_2 && age_1 < age_3) {
+//         console.log("Welcome") 
+//      } else if (age_1 > age_3) {
+//         console.log("Keep calm and look Culture channel")
+//      } else console.log("Technical work")}
+
+
+    //    checkAge(17)
+    //    checkAge(18)
+    //    checkAge(61)
+
+// Задание 1*. Преобразовать написанный код в 26-33 пунктах в функцию, принимающую на вход возраст.
+// Пример: const checkAge = function(age) {
+// Ваши преобразования
+// }
+// Вывести в консоль результат работы функции с возрастами 17, 18, 61
+
+let age_1
 
 let age_2 = 18
 
 let age_3 = 60
 
 let checkAge = function(age_1) {
-    if (age_1 < age_2) {
+        if (typeof age_1 == `number`) {
+     if (age_1 < age_2) {
         console.log(`“You don’t have access cause your age is ”` + age_1 + `“ It’s less then”`)
-    } else if (age_1 >= age_2 && age_1 < age_3) {
-        console.log("Welcome")
-    }}
+    }    else if (age_1 >= age_2 && age_1 < age_3) {
+        console.log("Welcome") 
+     }   else if (age_1 > age_3) {
+        console.log("Keep calm and look Culture channel")
+     }   else {console.log("Technical work")
+    }
+}
+}
 
-
-    checkAge(6)
-
-// if (age_1 < age_2)
-// {console.log(`“You don’t have access cause your age is ”` + age_1 + `“ It’s less then”`)}
-// // Если age_1 < age_2, вывести в консоль “You don’t have access cause your age is” + age_1 + “It’s less then”
-// else if (age_1 >= age_2, age_1 < age_3)
-// {console.log("Welcome")}
-// // Если age_1 >=  age_2 и age_1 <  age_3, вывести в консоль “Welcome!"
-//  else if 
-//  (age_1 > age_3){console.log("Keep calm and look Culture channe")}
-// //  Если age_1  > age_3, вывести в консоль “Keep calm and look Culture channel”.
-// else {console.log("Technical work")}
-// // Иначе выводите “Technical work”
-
-// const checkage = function(age) {
+     
+checkAge(`6`)
 
 
 
 
-// Преобразовать написанный код в 26-33 пунктах в функцию, принимающую на вход возраст.
-// Пример: const checkAge = function(age) {
-// Ваши преобразования
-// }
-// Вывести в консоль результат работы функции с возрастами 17, 18, 61
+
+     //   Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных.
+//   И если он не Number - кидалась ошибка.
