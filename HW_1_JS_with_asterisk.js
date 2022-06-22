d110542d3af7f5e0e39619157e8d41aa1ccdce15
avkpro2022 <avkpro@gmail.com>
2022-06-22 17:52:15 +0300

    The second task

--- HW_1_JS_with_asterisk.js
+++ HW_1_JS_with_asterisk.js
@@ -1,4 +1,4 @@
-// let age_1 = Number
+// let age_1 = Null
 
 // let age_2 = 18
 
@@ -9,9 +9,9 @@
 //         console.log(`“You don’t have access cause your age is ”` + age_1 + `“ It’s less then”`)
 //     } else if (age_1 >= age_2 && age_1 < age_3) {
 //         console.log("Welcome") 
-//      } else if (age_1 > age_3) {
+//     } else if (age_1 > age_3) {
 //         console.log("Keep calm and look Culture channel")
-//      } else console.log("Technical work")}
+//     } else console.log("Technical work")}
 
 
     //    checkAge(17)
@@ -24,31 +24,29 @@
 // }
 // Вывести в консоль результат работы функции с возрастами 17, 18, 61
 
-let age_1
+let = age_1 = null
 
 let age_2 = 18
 
 let age_3 = 60
 
 let checkAge = function(age_1) {
-        if (typeof age_1 == `number`) {
-     if (age_1 < age_2) {
+    if (typeof age_1 ==`number`) 
+    {    if (age_1 < age_2) {
         console.log(`“You don’t have access cause your age is ”` + age_1 + `“ It’s less then”`)
     }    else if (age_1 >= age_2 && age_1 < age_3) {
         console.log("Welcome") 
-     }   else if (age_1 > age_3) {
+     }  else if (age_1 > age_3) {
         console.log("Keep calm and look Culture channel")
-     }   else {console.log("Technical work")
-    }
+     }   else {console.log("Technical work")}
+    } else {console.log("error")
 }
 }
 
-     
-checkAge(`6`)
+checkAge(4)
+checkAge(18)
+checkAge(60)
+checkAge("dog")
 
-
-
-
-
-     //   Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных.
+//   Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных.
 //   И если он не Number - кидалась ошибка.
