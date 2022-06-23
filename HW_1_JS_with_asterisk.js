cc3a50e6240e8e2b58dd64081ed190541076b8aa
avkpro2022 <avkpro@gmail.com>
2022-06-23 17:50:44 +0300

    three task

--- HW_1_JS_with_asterisk.js
+++ HW_1_JS_with_asterisk.js
@@ -24,6 +24,34 @@
 // }
 // Вывести в консоль результат работы функции с возрастами 17, 18, 61
 
+// let = age_1 = null
+
+// let age_2 = 18
+
+// let age_3 = 60
+
+// let checkAge = function(age_1) {
+//     if (typeof age_1 ==`number`) 
+//     {    if (age_1 < age_2) {
+//         console.log(`“You don’t have access cause your age is ”` + age_1 + `“ It’s less then”`)
+//     }    else if (age_1 >= age_2 && age_1 < age_3) {
+//         console.log("Welcome") 
+//      }  else if (age_1 > age_3) {
+//         console.log("Keep calm and look Culture channel")
+//      }   else {console.log("Technical work")}
+//     } else {console.log("error")
+// }
+// }
+
+// checkAge(4)
+// checkAge(18)
+// checkAge(60)
+// checkAge("dog")
+
+//   Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных.
+//   И если он не Number - кидалась ошибка.
+
+
 let = age_1 = null
 
 let age_2 = 18
@@ -31,7 +59,10 @@ let age_2 = 18
 let age_3 = 60
 
 let checkAge = function(age_1) {
-    if (typeof age_1 ==`number`) 
+    age_1 = Number(age_1)
+    if (!isNaN(age_1))
+    console.log(age_1)
+    if (typeof age_1 ==`number`)
     {    if (age_1 < age_2) {
         console.log(`“You don’t have access cause your age is ”` + age_1 + `“ It’s less then”`)
     }    else if (age_1 >= age_2 && age_1 < age_3) {
@@ -43,10 +74,12 @@ let checkAge = function(age_1) {
 }
 }
 
+
 checkAge(4)
 checkAge(18)
-checkAge(60)
+checkAge(0)
 checkAge("dog")
 
-//   Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных.
-//   И если он не Number - кидалась ошибка.
+
+// Преобразовать 2* таким образом, чтобы значение '2'
+// (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number
