3467f13e2a4c7d415d0db657f5d4690d265c6646
avkpro2022 <avkpro@gmail.com>
2022-06-26 15:59:32 +0300

    updated with new

--- theory_js.js
+++ theory_js.js
@@ -1,66 +1,120 @@
-// let i = 50;   
-// while(i <= 100) {
-//    console.log(i);
-//    i++;
-// }
+let i = 50;   
+while(i <= 100) {
+   console.log(i);
+   i++;
+}
 
-// let i = 20;  
-// do {
-//     console.log(i);
-//   i++;
-// } while (i <= 30);
+// // let i = 20;  
+// // do {
+// //     console.log(i);
+// //   i++;
+// // } while (i <= 30);
  
-// for (let i = 200; i <= 250; i++) {
-//     console.log(i);
-//  }
+// // for (let i = 200; i <= 250; i++) {
+// //     console.log(i);
+// //  }
 
-//  //сложение с применением преобразования к числу
-// console.log(+'1' + +'3'); //получим значение: 4
+// //  //сложение с применением преобразования к числу
+// // console.log(+'1' + +'3'); //получим значение: 4
 
-// //декремент с постфиксной формой
-// let a = 3;
-// a++; 
-// console.log(a); //получим значение: 4
-
-
-// //инкремент с постфиксной формой
-// let b = 3;
-// b--; 
-// console.log(b); //получим значение: 2
+// // //декремент с постфиксной формой
+// // let a = 3;
+// // a++; 
+// // console.log(a); //получим значение: 4
 
 
-// //декремент с префиксной формой
-// a = 3;
-// console.log(++a); //получим значение: 4
+// // //инкремент с постфиксной формой
+// // let b = 3;
+// // b--; 
+// // console.log(b); //получим значение: 2
 
 
-// //инкремент с префиксной формой
-// b = 3;
-// console.log(--b); //получим значение: 2
+// // //декремент с префиксной формой
+// // a = 3;
+// // console.log(++a); //получим значение: 4
 
 
-// //возведение в степень
-// console.log(2 ** 3); //получим значение: 8
-// console.log(3 ** 2); //получим значение: 9
-
-// //остаток от деления:
-// console.log(11 % 2); //получим значение: 1
-// console.log(8 % 3); //получим значение: 2
+// // //инкремент с префиксной формой
+// // b = 3;
+// // console.log(--b); //получим значение: 2
 
 
+// // //возведение в степень
+// // console.log(2 ** 3); //получим значение: 8
+// // console.log(3 ** 2); //получим значение: 9
 
-let age = null
-
-let name = null
-
-let checkAge = function(age) {
+// // //остаток от деления:
+// // console.log(11 % 2); //получим значение: 1
+// // console.log(8 % 3); //получим значение: 2
 
 
-if(age >= 18) 
-    {console.log('Welcome')}
+
+// // let age = null
+
+// // let name = null
+
+// // let checkAge = function(age) {
+
+
+// // if(age >= 18) 
+// //     {console.log('Welcome')}
     
 
- else if (age < 18) {console.log('You are a baby')}
+// //  else if (age < 18) {console.log('You are a baby')}
 
-}
-checkAge(`55`)
+// // }
+// // checkAge(`55`)
+
+// // оператор switch
+
+// let arg = +prompt('Сколько?');
+
+// switch(arg) {
+//     case 1:
+//         alert('Значение переменной arg равно 1');
+//     break;
+
+//     case 3:
+//         alert('Значение переменной arg равно 3');
+//     break;
+
+//     case 5:
+//         alert('Значение переменной arg равно 5');
+//     break;
+
+//     default:
+//         alert('Значение переменной arg равно: ' + arg);
+// }
+
+// // Вызов функции:
+
+// function callPayment() {
+//     let amount = +prompt('На какую сумму Вы хотите пополнить счет?');
+//     alert(`Счет пополнен на сумму ${amount} $`);
+//  }
+ 
+//  callPayment();
+
+// //  или
+
+//  function callPayment(currency) {
+//     let amount = +prompt('На какую сумму Вы хотите пополнить счет?');
+//     alert(`Счет пополнен на сумму ${amount} ${currency}`);
+//  }
+ 
+ 
+//  let userCurrencyRub = '₽'; //валюта: рубль
+//  let userCurrencyUSD = '$'; //валюта: доллар
+ 
+//  callPayment(userCurrencyRub); //вызываем функцию с валютой: рубль
+//  callPayment(userCurrencyUSD); //вызываем функцию с валютой: доллар
+
+//  или
+
+//  function callPayment(currency =  '$') {
+//     let amount = +prompt('На какую сумму Вы хотите пополнить счет?');
+//     alert(`Счет пополнен на сумму ${amount} ${currency}`);
+//  }
+ 
+//  callPayment(); //применится валюта указанная по умолчанию
+//  callPayment('₽'); //применится валюта: рубль
