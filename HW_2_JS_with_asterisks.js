cfb3dc7687863cd186e15ba61aaad67864e8a726
avkpro2022 <avkpro@gmail.com>
2022-07-09 16:00:56 +0300

    Task with smile and stroka

--- HW_2_JS_with_asterisks.js
+++ HW_2_JS_with_asterisks.js
@@ -27,15 +27,22 @@
 // :):):):)
 // :):):):):)
 
-const arr = [`:)`, `:):)`, `:):):)`, `:):):):)`, `:):):):):)`,];
-function () => {let i = 0; i < arr.length; i++
-}
-console.log()
+// function printSmile(stroka, numberOfRows) {
+//     let result = "";
+//     for (let i = 0; i < numberOfRows; i++) {
+//         result += stroka
+//         console.log(result)
+//     }
+// }
+// printSmile(":)", 4)
 
 // 2*. Преобразовать 2 задачу в функцию, принимающую на вход строку,
 // которая и будет выводиться в консоль (как в условии смайлик), а также количество строк для вывода 
 // e.g. function printSmile(stroka, numberOfRows)
 
+function getWordStructure(word) {
+
+}
 
 // 3**.  Написать функцию, которая принимает на вход слово. Задача функции посчитать и вывести в консоль, сколько в слове гласных, и сколько согласных букв.
 // e.g. function getWordStructure(word)
