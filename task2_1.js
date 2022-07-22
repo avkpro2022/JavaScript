c5d190b7fc243d8f7e4714fdcf416df6b491fbfd
avkpro2022 <avkpro@gmail.com>
2022-07-22 12:31:50 +0300

    add the file

--- task2_1.js
+++ task2_1.js
@@ -1,18 +1,24 @@
 // Task 2*** Реализуйте считывание из JSONки из файла task2.json с помощью,
 // например, модуля fs. для дальнейшего использования в функции, описанной в задании
 
+
+//  First variant
 const fs = require("fs");
-let user = JSON.parse(fs.readFileSync("task2.json"))
+let user = JSON.parse(fs.readFileSync("/Users/apple/qa_course/JavaScript/task2.json"));
 
-function uniquiFuncl() {
-    let uniquel = [];
-    user.forEach(el => {
-        if(!uniquel.includes(JSON.stringify(el))) uniquel.push(JSON.stringify(el))
-    })
- return uniquel.map(el => JSON.parse(el))
+// function uniquiFuncl() {
+//     let uniquel = [];
+//     user.forEach(el => {
+//         if(!uniquel.includes(JSON.stringify(el))) uniquel.push(JSON.stringify(el))
+//     })
+//  return uniquel.map(el => JSON.parse(el))
 
-}
-console.log(uniquiFuncl())
 
-// let uniquel = Array.from(naw.Set(user.map((item) => JSON.stringify(item)))).map((item) => JSON.parse(item));
-// console.log(unique);
+// }
+// console.log(uniquiFuncl())
+
+// Second variant  
+
+
+let uniquel = Array.from(new Set(user.map((item) => JSON.stringify(item)))).map((item) => JSON.parse(item));
+console.log(uniquel);
